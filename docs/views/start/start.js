e60794f227ff99564fc21c840947d9686e893aa0
Linus <liljalinus@gmail.com>
2019-05-21 22:50:56 +0200

    ..

--- docs/views/start/start.js
+++ docs/views/start/start.js
@@ -1,5 +1,4 @@
 import Lily from 'https://unpkg.com/lily@0.2.2/index.js'
-import syntax from './../../../lil-syntax.js'
 
 import Hero from './components/hero.js'
 import Intro from './components/intro.js'
